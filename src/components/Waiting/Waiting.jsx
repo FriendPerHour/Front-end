8adc02d37cf8fc3ffd49b3a401aa53680a1242f6
mohamedsafwat5819-a11y <mohamedsafwat5819@gmail.com>
2025-09-24 21:38:15 +0300

    Frontend Waiting.jsx

--- src/components/Waiting/Waiting.jsx
+++ src/components/Waiting/Waiting.jsx
@@ -9,7 +9,42 @@ export default function Waiting() {
 
   return (
     <>
-      <h1>Template Name</h1>
+      <!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>انتظار رد المتطوع</title>
+
+  <!-- إضافة Tailwind CSS -->
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <style>
+    body {
+      font-family: 'Tajawal', sans-serif;
+    }
+  </style>
+</head>
+
+<body class="bg-white min-h-screen flex flex-col items-center justify-center">
+
+  <!-- المحتوى داخل الصفحة -->
+  <div class="text-center p-2">
+    <div class="flex justify-center ">
+      <img src="images/waiting.png" alt="وقت الانتظار" class="max-w-xs max-h-x">
+    </div>
+
+    <p class="text-4xl font-semibold bg-gradient-to-r from-[#0D8EFF] to-[#00FF84] bg-clip-text mt-4 mb-6 text-transparent p-4">
+  من فضلك انتظر رد المتطوع
+   </p>
+
+
+  </div>
+
+</body>
+
+</html>
     </>
   );
 }
